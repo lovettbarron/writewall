4fa43263c308cdfd16301c4bc6890fa8f2ba44c5
Andrew Lovett Barron <andrewlb@Andrew-Lovett-Barrons-MacBook-Air.local>
2012-01-31 19:01:57 -0500

    Revised

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -19,7 +19,7 @@ socket.on('msg', function(data) {
 
 socket.on('success', function(data) {
     console.log(data);
-		$('#input').append('')
+		$('#input').append('<p>' + data.msg + '</p>')
   });
 
 
